451f98c39adaaca057c3346a1ca86bc430195ac7
Horst <horst1wj@AUX-d03437.local>
2016-06-09 14:26:59 -0400

    Added all of the Models, ModelForms, and Tuples
       Both the ShirtOptions Model class and the FrisbeeOptions class were added to allow multiple checkboxes on the ModelForm.
    
       The Person Class Model includes every single field that is to be used on the form. Every single field is a CharField, except for shirt_options and frisbee_options.
       Both of those are ManytoManyFields.
    
       The ModelForm Class PersonForm allows the Models to be displayed as a form. shirt_options and frisbee_options are declared as ModelMultipleChoiceFields and displayed
       as a CheckboxSelectMultiple widget. PersonForm also includes the Meta Class, which holds all non-fields.

--- register/models.py
+++ register/models.py
@@ -2,49 +2,84 @@ from __future__ import unicode_literals
 from django import forms
 from django.db import models
 from django.forms import ModelForm
+import re
+from django import forms
+import localflavor.us.models
 
-# Create your models here
+SHIRT = "Shirt"
+FRISBEE = "Frisbee"
 
-#CHOICES is simply the user's choice for the sex_field
-#CHOICES = (('1','Male'),('2','Female'))
+SMALL = 'S'
+MEDIUM = 'M'
+LARGE = 'L'
 
-#first_name is a text field that allows our user to enter their first name
-'''
-class first_name(models.Model):
+MALE = 'M'
+FEMALE = "F"
+PREF = "Pref"
 
-    first_name_field = models.charField(max_length = 200)
+#Every single dropdown CharField requires tuples.
 
-#last_name is a text field that allows our user to enter their last name
-class last_name(models.Model):
+CHOICES = (
+    ('1','Male'),
+    ('2','Female'),
+)
 
-    last_name_field = models.charField(max_length = 200)
+BUY_OPTIONS_LIST = (
+    (SHIRT, 'shirt'),
+    (FRISBEE,'frisbee'),
+)
 
-#email is a field that allows our user to enter their email account
-class email(models.Model):
+SHIRT_CHOICE = (
+    (SMALL, 'S'),
+    (MEDIUM,'M'),
+    (LARGE,'L'),
+)
 
-    email_field = models.charField(max_length = 300)
+SEX_CHOICE = (
+    (MALE, 'Male'),
+    (FEMALE,'Female'),
+    (PREF,'Prefer Not to Respond'),
+)
 
-#phone_number is a field that allows the user to enter their phone number
-class phone_number(models.Model):
+class ShirtOptions(models.Model):
+    description = models.CharField(max_length=100)
+    def __str__(self):
+        return self.description
 
-    phone_number_field = models.charField(max_length = 50)
-
-#first_name is a radio widget that allows our user to pick their gender
-class sex(models.Model):
-
-    sex_field = forms.ChoiceField(widget=forms.RadioSelect, choies = CHOICES)
-
-
-class skill(forms.ModelForm):
-
-    skill_drop_down = forms.ChoiceField(choices =[(x, x) for x in range(1,6)])
-
-
-class buy_options(models.Model):
-
-
-class shirt_size(models.Model):
+class FrisbeeOptions(models.Model):
+    description = models.CharField(max_length=300)
+    def __str__(self):
+        return self.description
 
 
-class position(models.Model):
-'''
+#Person contains the information that our user fills out in anticipation of the event
+class Person(models.Model):
+
+    #The next four lines are regular text boxes
+    first_name = models.CharField(max_length = 200)
+    last_name = models.CharField(max_length = 200)
+    email = models.CharField(max_length = 200)
+    phone_number = localflavor.us.models.PhoneNumberField()
+
+    #These two lines are for the two checkboxes
+    shirt_options = models.ManyToManyField(ShirtOptions)
+    frisbee_options = models.ManyToManyField(FrisbeeOptions)
+
+    #The following charfields will appear as dropdown boxes
+    sex = models.CharField(max_length = 100, choices=SEX_CHOICE,)
+    skill = models.CharField(max_length = 200, choices=((str(x), x) for x in range(1, 6)), default = 1,)
+    shirt_size = models.CharField(max_length = 200,choices = SHIRT_CHOICE,default = 'Small',)
+
+
+
+
+#PersonForm is simply a ModelForm for the Person class
+class PersonForm(ModelForm):
+
+    #These next two lines will appear as multiple checkboxes
+    shirt_options = forms.ModelMultipleChoiceField(queryset=ShirtOptions.objects.all(), required=False, widget=forms.CheckboxSelectMultiple)
+    frisbee_options = forms.ModelMultipleChoiceField(queryset=FrisbeeOptions.objects.all(), required=False, widget=forms.CheckboxSelectMultiple)
+
+    class Meta:
+        model = Person
+        fields = '__all__'
