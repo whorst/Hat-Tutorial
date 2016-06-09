ec50ac6ad972e4120b99e61512c3310eb574c8a3
Horst <horst1wj@AUX-d03437.local>
2016-06-09 14:09:08 -0400

    Registered Every new Model
    I registered the ShirtOptions model and the FrisbeeOptions model.

--- register/admin.py
+++ register/admin.py
@@ -1,1 +1,7 @@
 from django.contrib import admin
+from models import Person, ShirtOptions, FrisbeeOptions
+
+# This will register your person Model and create a corresponding template on the admin page 
+admin.site.register(Person)
+admin.site.register(ShirtOptions)
+admin.site.register(FrisbeeOptions)
