c893296d67bffe0262ec3fea89ea582aa516619a
Horst <horst1wj@AUX-d03437.local>
2016-06-09 14:46:33 -0400

    Added the Function for the Models
       The register function can now handle the  ModelForm PersonForm and display it to the reg.html template.
       This function also checks if the user is attempting to post information to the database. If so, it also checks to make
       Sure that all required fields are properly filled out. If Everything is filled out properly, the user is redirected to the
       thanks.html template. Otherwise, the user will have to resubmit the form.

--- register/views.py
+++ register/views.py
@@ -1,21 +1,30 @@
 from django.shortcuts import render
 from django.views import generic
-
+import models
+from models import PersonForm, Person
+from django.core.urlresolvers import reverse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, HttpResponse
 # Create your views here.
-'''
-class Register(generic.DetailView):  #look at different views
-    template_name = 'register/reg.html'
 
-    def get_queryset(self):
-        print("Hello!")
-'''
-
+#This is our home url
 def home(request):
-    return render(request, "register/home.html",{})
+    return render(request, "register/home.html", {})
 
-
+#This is the url we use for registration
 def register(request):
-    return render(request, "register/reg.html",{})
+    personform = PersonForm()
+    if request.method=='POST':
+        personform = PersonForm(request.POST)
+        if personform.is_valid():
+            profile = personform.save(commit=False)
+            profile.save()
+            personform.save_m2m()
+            return HttpResponseRedirect(reverse('thanks'))
+        else:
+            personform = PersonForm()
+    return render(request, "register/reg.html", {'PersonForm': personform})
 
+#This is the url thank you screen
 def thanks(request):
     return render(request, "register/thanks.html",{})
