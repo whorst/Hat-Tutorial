a5e30cd60b81f9b4f24c7f5a3619e8746641c9c9
Horst <horst1wj@AUX-d03437.local>
2016-06-13 09:26:35 -0400

    Removed Unnecessary Comments and Commented the classes ShirtOptions and FrisbeeOptions
       ShirtOptions and Frisbee Options were both uncommented, so I was sure to comment both of them.
       There was also some commented code sitting in my porject taking up space, so I got rid of that
       as well.

--- register/models.py
+++ register/models.py
@@ -4,31 +4,31 @@ from django.db import models
 from django.forms import ModelForm
 import re
 from django import forms
-import localflavor.us.models
+from localflavor.us.models import PhoneNumberField
 
-SHIRT = "Shirt"
-FRISBEE = "Frisbee"
+SHIRT = 'Shirt'
+FRISBEE = 'Frisbee'
 
 SMALL = 'S'
 MEDIUM = 'M'
 LARGE = 'L'
 
 MALE = 'M'
-FEMALE = "F"
-PREF = "Pref"
+FEMALE = 'F'
+PREF = 'Pref'
 
-IN_CUTTER = "In Cutter"
+IN_CUTTER = 'In Cutter'
 OUT_CUTTER = 'Out Cutter'
 SHORT_DISTANCE_HANDLER = 'Short Distance Handler'
 LONG_DISTANCE_HANDLER = 'Long Distanc Handler'
 #Every single dropdown CharField requires tuples.
 
 SKILL_CHOICES = (
-    ('1','1,   Amateur'),
+    ('1','1   Amateur'),
     ('2','2'),
     ('3','3'),
     ('4','4'),
-    ('5','5,   Pro'),
+    ('5','5   Pro'),
 )
 
 BUY_OPTIONS_LIST = (
@@ -66,14 +66,17 @@ class FrisbeeOptions(models.Model):
         return self.description
 
 
-#Person contains the information that our user fills out in anticipation of the event
+
 class Person(models.Model):
+    """
+    Person contains the information that our user fills out in anticipation of the event
+    """
 
     #The next four lines are regular text boxes
     first_name = models.CharField(max_length = 200)
     last_name = models.CharField(max_length = 200)
     email = models.CharField(max_length = 200)
-    phone_number = localflavor.us.models.PhoneNumberField()
+    phone_number = models.CharField(max_length = 13)
 
     #These two lines are for the two checkboxes
     shirt_options = models.ManyToManyField(ShirtOptions)
@@ -81,19 +84,19 @@ class Person(models.Model):
 
     #The following charfields will appear as dropdown boxes
     sex = models.CharField(max_length = 100, choices=SEX_CHOICE,)
-    skill = models.CharField(max_length = 200, choices= SKILL_CHOICES, default = "1",)
+    skill = models.CharField(max_length = 200, choices= SKILL_CHOICES, default = '1',)
     shirt_size = models.CharField(max_length = 200,choices = SHIRT_CHOICE,default = 'Small',)
     position = models.CharField(max_length = 200,choices = POSITION_CHOICE,default = 'In Cutter',)
 
+    def __str__(self):
+        return('{first} {last}'.format(first=self.first_name, last=self.last_name))
+        # return(' '.join((self.first_name, self.last_name)))
 
+    def valid_phone_number(self):
+        pattern = re.compile('^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$')
+        j = pattern.match(self.phone_number)
 
-# #PersonForm is simply a ModelForm for the Person class
-# class PersonForm(ModelForm):
-#
-#     #These next two lines will appear as multiple checkboxes
-#     shirt_options = forms.ModelMultipleChoiceField(queryset=ShirtOptions.objects.all(), required=False, widget=forms.CheckboxSelectMultiple)
-#     frisbee_options = forms.ModelMultipleChoiceField(queryset=FrisbeeOptions.objects.all(), required=False, widget=forms.CheckboxSelectMultiple)
-#
-#     class Meta:
-#         model = Person
-#         fields = '__all__'
+        if j:
+            return True
+        else:
+            return False
