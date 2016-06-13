b030c5eb82d2fbbc9a191a41f5597d79c0e43d93
Horst <horst1wj@AUX-d03437.local>
2016-06-13 09:31:28 -0400

    Test if  .valid_phone_number Catches Invalid Phone Numbers
       Added a test to see if the Person method valid_phone_number will actually catch bad phone numbers.

--- register/tests.py
+++ register/tests.py
@@ -1,1 +1,20 @@
 from django.test import TestCase
+
+from models import Person
+
+
+class PersonTest(TestCase):
+
+    def test_invalid_phone_number_is_identified(self):
+        '''
+        Will test that there an entry of an invalid phone number into the respective text box will return False.
+        '''
+
+        person = Person(phone_number = '734-854-8704')
+        self.assertEqual(person.valid_phone_number(), True)
+
+
+
+
+def create_person(phone_number_test):
+    return Person.objects.create(phone_number = phone_number_test)
