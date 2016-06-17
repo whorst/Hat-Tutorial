4380317ed07db495a7209b15acc0ade40e132ce6
Horst <horst1wj@AUX-d03437.local>
2016-06-17 10:06:25 -0400

    Changing my Admin File, Adding a Form File, Making Changes to the Model, Changing the Test File, Adding to the Form File, and Adding the Migrations File
    	Unfortunately, I heven't made a Commit in quite a while.

--- register/tests.py
+++ register/tests.py
@@ -1,20 +1,22 @@
 from django.test import TestCase
 
 from models import Person
-
+from forms import valid_phone_number
 
 class PersonTest(TestCase):
 
+
     def test_invalid_phone_number_is_identified(self):
         '''
-        Will test that there an entry of an invalid phone number into the respective text box will return False.
+        Will test that an entry of an invalid phone number into the respective text box will return False.
         '''
 
-        person = Person(phone_number = '734-854-8704')
-        self.assertEqual(person.valid_phone_number(), True)
+        self.assertEqual(valid_phone_number('73-8704'), False)
 
 
+    def test_valid_phone_number_is_identified(self):
+        '''
+        Will test that an entry of an valid phone number into the respective text box will return True.
+        '''
 
-
-def create_person(phone_number_test):
-    return Person.objects.create(phone_number = phone_number_test)
+        self.assertEqual(valid_phone_number('734-111-8704'), True)
